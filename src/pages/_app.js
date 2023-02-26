c46c26092096c801742cd1ae776888e62fd5a387
Sam Hutton <samuelhutton00@gmail.com>
2023-02-26 18:10:17 +1000

    actual mvp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,16 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
 import '../styles/Home.css'
+import '../styles/Medium.css'
+
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
+      <Component {...pageProps} />
+    </>)
 }
